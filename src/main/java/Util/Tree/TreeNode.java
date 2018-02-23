06c82e0465f1cd6694f9fa0c6f6244438a775f4b
dongyv <529979142@qq.com>
2018-02-23 15:42:54 +0800

    树 面试题

---
+++ src/main/java/Util/Tree/TreeNode.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) <a href="http://git.oschina.net/eason_zhangfei">zhangfei_eason</a>.
+ */
+package com.dql.cla.service;
+
+/**
+ * @author http://git.oschina.net/xiachenhang
+ * @date 2018-02-23
+ */
+//首先建立一个二叉树类
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { 
+     val = x; 
+   }
+}
